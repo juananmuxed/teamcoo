bb830c4b72ea7ae4675dc3f84c8f595976c7ba45
Juanan <juan-antonio.sanchez@necomplus.com>
2021-10-25 21:40:06 +0200

    🚽 Upload files fix 01

--- api/routes/files.js
+++ api/routes/files.js
@@ -2,10 +2,19 @@ const crypto = require('crypto')
 const multer = require('multer')
 const GridFsStorage = require('multer-gridfs-storage')
 const path = require('path')
+require('dotenv').config({ path: require('find-config')('.env') });
+
+const {
+    DATABASE_NAME,
+    DATABASE_HOST,
+    DATABASE_PORT,
+    NODE_ENV,
+    WEB_NAME
+} = process.env;
 
 const express = require("express")
-const router = express.Router()
-const url = 'mongodb://localhost:27017/teamcoo'
+const router = express.Router();
+const url = `mongodb://${NODE_ENV == 'production' ? WEB_NAME + '-db' : DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`;
 const filesController = require('../controllers/files')
 
 // Create storage engine
@@ -20,11 +29,11 @@ const storage = new GridFsStorage({
                 }
                 let bucketName
                 let filename
-                if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/gif'){
+                if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/gif') {
                     filename = 'image_' + buf.toString('hex') + '_' + Date.now() + path.extname(file.originalname)
                     bucketName = 'images'
                 }
-                else{
+                else {
                     filename = 'upload_' + buf.toString('hex') + '_' + Date.now() + path.extname(file.originalname)
                     bucketName = 'uploads'
                 }
